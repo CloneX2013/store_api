31b1b7f7c1e242ae99bcdc1589c03de81580dbe4
Pedro Paulo Dantas de Menezes <49175112+CloneX2013@users.noreply.github.com>
2025-07-16 13:24:54 -0300

    Update exceptions.py
    Novo: Exceção para erros de inserção

--- store/core/exceptions.py
+++ store/core/exceptions.py
@@ -8,3 +8,19 @@ class BaseException(Exception):
 
 class NotFoundException(BaseException):
     message = "Not Found"
+
+class BaseException(Exception):
+    """Base exception class for the application."""
+    message: str = "Internal Server Error"
+
+    def __init__(self, message: str | None = None) -> None:
+        if message:
+            self.message = message
+
+class NotFoundError(BaseException):
+    """Exception for data not found."""
+    message = "Not Found"
+
+class InsertionError(BaseException):
+    """Exception for data insertion errors."""
+    message = "Error while inserting data"
